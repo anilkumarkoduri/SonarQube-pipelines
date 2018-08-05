418850b73f47128e4c06a1b7304b74f729ef221b
anilkumarkoduri <41462880+anilkumarkoduri@users.noreply.github.com>
2018-08-05 09:21:46 +0530

    Update Tests.java

--- src/test/java/com/pattabhi/home/tests/Tests.java
+++ src/test/java/com/pattabhi/home/tests/Tests.java
@@ -4,7 +4,7 @@ public class Tests {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.println("Test executed successfully..");
+   System.out.println("Test executed successfully after many iterations..");
 	}
 
 }
